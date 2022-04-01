2d4600d6bc51684afd94334943ae22eb1a9950b6
KevinDaPlayer <325881@gmail.com>
2022-04-02 00:12:03 +0800

    Work1

--- src/main/java/com/Kevin/ticket/Tester.java
+++ src/main/java/com/Kevin/ticket/Tester.java
@@ -1,7 +1,18 @@
 package com.Kevin.ticket;
 
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
-        Ticket ticket = new Ticket(Station.TAIPEI_STATION, Station.KOAHSIUNG_STATION);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Start station?[1. Taipei, 2. Taichung, 3.Kaohsiung]");
+        int choice1 = Integer.parseInt(scanner.next());
+        Station start = null;
+
+
+
+
+
+
     }
 }
--- src/main/java/com/Kevin/ticket/Ticket.java
+++ src/main/java/com/Kevin/ticket/Ticket.java
@@ -7,8 +7,32 @@ public class Ticket {
     int price;
     Station start;
     Station destination;
-    public Ticket (Station start, Station destination ) {
+    public Ticket (Station start, Station destination  ) {
         this.start = start;
         this.destination = destination;
+        if (start == Station.TAIPEI_STATION) {
+            if (destination == Station.KOAHSIUNG_STATION) {
+                price = 1500;
+            } else {
+                price = 600;
+            }
+        } else if (start == Station.TAICHUNG_STATION) {
+            if (destination == Station.KOAHSIUNG_STATION) {
+                price = 900;
+            } else {
+                price = 600;
+            }
+        } else if (start == Station.KOAHSIUNG_STATION) {
+            if (destination == Station.TAICHUNG_STATION) {
+                price = 900;
+            } else {
+                price = 600;
+            }
+        }
+    }
+
+    public void print () {
+        System.out.println(start.name + "\t" + destination.name + "\t" + "price:" + price);
     }
 }
+
