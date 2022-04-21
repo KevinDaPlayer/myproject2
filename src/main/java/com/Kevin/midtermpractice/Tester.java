223425fc641660e08774fba5962499e5b5790595
KevinDaPlayer <325881@gmail.com>
2022-04-21 20:07:59 +0800

    midterm practice

---
+++ src/main/java/com/Kevin/midtermpractice/Tester.java
@@ -0,0 +1,61 @@
+package com.Kevin.midtermpractice;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Tester {
+    public static void main(String[] args) {
+        ArrayList<Ticket> list = new ArrayList<>();
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("your start station? 1 2 3");
+        int choice = Integer.parseInt(scanner.next());
+        Station start = null;
+        switch (choice) {
+            case 1:
+                start = Station.TAIPEI_STATION;
+                break;
+            case 2:
+                start = Station.TAICHUNG_STATION;
+                break;
+            case 3:
+                start = Station.KOAHSHUNG_STATION;
+                break;
+        }
+        System.out.println("your destination ? 123");
+        choice = Integer.parseInt(scanner.next());
+        Station destination = null;
+        switch (choice) {
+            case 1:
+                destination = Station.TAIPEI_STATION;
+                break;
+            case 2:
+                destination = Station.TAICHUNG_STATION;
+                break;
+            case 3:
+                destination = Station.KOAHSHUNG_STATION;
+                break;
+        }
+        System.out.println("ticket type? 123");
+        choice = Integer.parseInt(scanner.next());
+        Ticket ticket = null;
+        switch (choice) {
+            case 1:
+                ticket = new Ticket(start, destination);
+                list.add(ticket);
+                break;
+            case 2:
+                ticket = new StudentTicket(start, destination);
+                list.add(ticket);
+                break;
+        }
+        list.get(0).print();
+        ticket.print();
+        for (Ticket t : list) {
+            t.print();
+        }
+
+    }
+}
+
+
+
---
+++ src/main/java/com/Kevin/midtermpractice/Ticket.java
@@ -0,0 +1,41 @@
+package com.Kevin.midtermpractice;
+
+public class Ticket {
+    public static final int TAIPEI_STATION = 100;
+    public static final int TAICHUNG_STATION = 200;
+    public static final int KAOHSIUNG_STATION = 300;
+
+    Station start;
+    Station destination;
+    int price;
+    public Ticket (Station start, Station destination) {
+        this.start = start;
+        this.destination = destination;
+        if (start == Station.TAIPEI_STATION) {
+            if (destination == Station.TAICHUNG_STATION) {
+                price = 600;
+            } else {
+                price = 1500;
+            }
+        }
+        if (start == Station.TAICHUNG_STATION) {
+            if (destination == Station.TAIPEI_STATION) {
+                price = 600;
+            } else {
+                price = 900;
+            }
+        }
+        if (start == Station.KOAHSHUNG_STATION) {
+            if (destination == Station.TAICHUNG_STATION) {
+                price = 900;
+            } else {
+               price = 1500;
+            }
+        }
+    }
+    public  void print () {
+        System.out.println(start.name + "\t" + destination.name + "\t" + price);
+    }
+
+}
+
