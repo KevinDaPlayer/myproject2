b9770ef93620d54f027bd4c66b941c499a8da86a
KevinDaPlayer <325881@gmail.com>
2022-05-18 22:24:02 +0800

    parking fees1 and fees2 3:14 => NTD$90 , 3:15 => NTD$120 3:15-3:30 => NTD$105

--- src/main/java/com/Kevin/parking/Runner.java
+++ src/main/java/com/Kevin/parking/Runner.java
@@ -26,12 +26,18 @@ public class Runner {
            int mins = (int)(d2.getTime()-d.getTime())/1000/60;
            float f = mins%60;
            System.out.println(f);
-           int fees = 30*(mins/60);
+           int fees1 = 30*(mins/60);
            // 3:14 nt90 3:15 nt 120
            if (f >= 15) {
-               fees = fees + 30;
+               fees1 = fees1 + 30;
            }
-           System.out.println(fees);
+           int fees2 = 30*(mins/60);
+           if (f >= 15 && f <= 30) {
+               fees2 = fees2 + 15;
+           } else if (f > 30) {
+               fees2 = fees2 + 30;
+           }
+           System.out.println("fees1 :" + fees1 + "\t" + "fees2 :" + fees2);
        } catch (Exception e) {
            System.out.println("Wrong Format");
 
