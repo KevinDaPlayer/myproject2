39b5151efcd2500208d7b3d0ebd05689ecafc80c
KevinDaPlayer <325881@gmail.com>
2022-05-20 16:28:47 +0800

    frame actionlistener practice

---
+++ src/main/java/com/Kevin/ui/MyWin.java
@@ -0,0 +1,28 @@
+package com.Kevin.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyWin {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(600, 400);
+        frame.setLocation(300, 200);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JButton button = new JButton("ok");
+        MyActionListener actionListener = new MyActionListener();
+        button.addActionListener(new ActionListener() {
+            @Override
+            public  void actionPerformed (ActionEvent actionEvent) {
+                System.out.println("HelloWorld!!!");
+            }
+        });
+        frame.setLayout(new FlowLayout());
+        frame.add(button);
+        frame.add(new JButton("123"));
+        System.out.println("END");
+    }
+}
