dde2ab68180b758f0b517e2129881ee62be41e7c
KevinDaPlayer <325881@gmail.com>
2022-05-27 16:27:26 +0800

    GuessFrame 0527class

--- src/main/java/com/Kevin/ui/GuessFrame.java
+++ src/main/java/com/Kevin/ui/GuessFrame.java
@@ -4,11 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame {
 
-    JLabel label = new JLabel("shitON");
-    JButton button = new JButton("hi");
+    JLabel label = new JLabel("GOODLUCK");
+    JButton button = new JButton("GUESS");
+    JTextField number = new JTextField(8);
+    Random random = new Random();
+    int secret = random.nextInt(10)+1;
     public GuessFrame () {
         super();
         setSize(600, 400);
@@ -18,10 +22,20 @@ public class GuessFrame extends JFrame {
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                label.setText("Sheeesh");
+                System.out.println(secret);
+                int num = Integer.parseInt(number.getText());
+                if (num > secret) {
+                    label.setText("Smaller");
+                } else if (num < secret) {
+                    label.setText("Bigger");
+                } else {
+                    label.setText("Bingo");
+                }
+                //label.setText("Sheeesh");
             }
         });
         setLayout(new FlowLayout());
+        add(number);
         add(button);
         add(label);
         setVisible(true);
